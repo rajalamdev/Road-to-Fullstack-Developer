d42c5e45804b36ccd9eb9ed6ae3e713d13e54bef
rajalamofficial <rajalamofficial@gmail.com>
2022-09-24 13:21:04 +0700

    update

---
+++ Frontend/My Projects/Fullstack Blog (Next JS)/my-app/components/Layout.jsx
@@ -0,0 +1,19 @@
+import Container from "./Container";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }) {
+  return (
+    <Container>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Container>
+  )
+}
---
+++ Frontend/My Projects/Fullstack Blog (Next JS)/my-app/pages/detail.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function DetailPost() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  )
+}
